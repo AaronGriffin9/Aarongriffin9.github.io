3bd343532862c32c82fa57456ad0616fd0b14a3e
AaronGriffin9 <44605619+AaronGriffin9@users.noreply.github.com>
2021-02-24 08:53:58 +0000

    Update script.js

--- Templates/Auroa Grove/Assets/Scripts/script.js
+++ Templates/Auroa Grove/Assets/Scripts/script.js
@@ -1,14 +1,32 @@
 window.onload = function() {
   nextSlide();
 };
+
 let slides = document.querySelectorAll('.slider-cont .slide');
 let currentSlide = 0;
 let slideInterval = setInterval(nextSlide, 5000);
 const bar = document.getElementById('toggle1');
 
+let modal = document.getElementById('modal');
+const modal_btn = document.getElementById('modal-btn');
+const modal_close_btn = document.getElementById('close-btn');
+let close = document.getElementById("close");
+
 bar.addEventListener("click", function(){
   const menu = document.getElementById('nav');
   menu.classList.toggle("display");
+});
+
+modal_btn.addEventListener("click", function(){
+  modal.style.display = "block";
+});
+
+ modal_close_btn.addEventListener("click", function(){
+  modal.style.display = "none";
+});
+
+close.addEventListener("click", function(){
+  modal.style.display = "none";
 });
 
 function nextSlide() {
